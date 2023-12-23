70bcc2152f6994333d52754ade156a24f3492af5
finbarr12 <ogbonnafinbarr@gmail.com>
2023-12-23 21:58:12 +0100

    added

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-
+from .controller.TaskController import todoroute
+from .models.TaskModel import db
 
 app  = Flask(__name__)
 
+app.config('MONGODB_SETTINGS'):{
+    "host": "mongodb://127.0.0.1/tododb"
+}
 
+db.init_app(app)
+
+
+app.register_blueprint(todoroute)
 
 
 
